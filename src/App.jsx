7c12c9f5b60760cac30838dc5aa507b6fa91e888
meirman_is_creator <sarsenbaymeyirman@gmail.com>
2024-07-05 10:45:24 +0500

    fix: response.data

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,8 @@ import { UserContextProvider } from "./UserContext";
 import PlacesPage from "./pages/PlacesPage";
 import PlacePage from "./pages/PlacePage";
 import FindRoommateFormPage from "./pages/FindRoommateFormPage";
-axios.defaults.baseURL = "https://turamyzba-back-end-production.up.railway.app";
-// axios.defaults.baseURL = "http://localhost:4000";
+// axios.defaults.baseURL = "https://turamyzba-back-end-production.up.railway.app";
+axios.defaults.baseURL = "http://localhost:4000";
 axios.defaults.withCredentials = true;
 function App() {
   return (
